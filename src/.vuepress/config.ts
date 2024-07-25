dc12c2b29774c41c63abf3d25bf4c210db698bc1
wuhaocheng29 <wu2305790321@gmail.com>
2024-07-25 22:20:29 +0800

    Add search plugin in .vuepress/config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,4 +1,5 @@
 import { defineUserConfig } from "vuepress";
+import { searchProPlugin } from "vuepress-plugin-search-pro";
 
 import theme from "./theme.js";
 
@@ -10,6 +11,17 @@ export default defineUserConfig({
   description: "vuepress-theme-hope 的文档演示",
 
   theme,
+  
+  plugins: [
+    searchProPlugin (
+      {
+        indexContent: true,
+        autoSuggestions: true,
+        queryHistoryCount: 0,
+        resultHistoryCount: -1
+      }
+    ),
+  ],
 
   // 和 PWA 一起启用
   // shouldPrefetch: false,
